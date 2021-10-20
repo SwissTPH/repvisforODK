019e65f60d3b405390c54a0fdb8188d9c5d5c610
Lucas Silbernagel <77398086+lucidviews@users.noreply.github.com>
2021-10-20 09:53:57 +0200

    New function collection_period()
    The function finds the earliest and the latest submission dates in a data frame.

---
+++ R/collection_period.R
@@ -0,0 +1,19 @@
+#' Get the earliest and latest of your data
+#'
+#' The function finds the earliest and the latest submission dates in a data frame. It accounts for different spellings of the submission date column
+#' by applying a case-insensitive RegEx formula that matches every combination of the words 'submission' and 'date'.
+#'
+#' @param df Data frame containing the ODK data that is to be used.
+#'
+#' @return list
+#' @export
+#'
+#' @examples
+#' placeholder
+collection_period <- function(df){
+  submission_date_col = colnames(df)[grepl('submission.*date', colnames(df), ignore.case = T)|grepl('date.*submission', colnames(df), ignore.case = T)]
+  earliest_submission = as.Date(min(df[[submission_date_col]]))
+  latest_submission = as.Date(max(df[[submission_date_col]]))
+
+  return(list(earliest_submission, latest_submission))
+}
